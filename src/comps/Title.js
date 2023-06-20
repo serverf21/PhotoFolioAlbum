9b642a0f407d1c220746f1ff01ff65fffb6c8626
Sarvagya Saxena <sarvagyasaxena.2102@gmail.com>
2023-06-20 19:06:28 +0530

    Made final changes to the UI

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,7 +4,10 @@ import '../index.css'
 const Title = () => {
     return (
         <div className="title">
-            <h1>PhotoFolio</h1>
+            <div className='header'>
+                <img src="https://cdn-icons-png.flaticon.com/512/3342/3342176.png" alt="" />
+                <h1>PhotoFolio</h1>
+            </div>
             <h2>Your Pictures</h2>
             <p>Tap on the "+" to add a new picture</p>
         </div>
